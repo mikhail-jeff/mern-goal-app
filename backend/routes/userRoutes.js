d2f5f0db6b598aa1e30d58f722b6d01ae4277d15
jeffmikhail <mikhailcruz78@gmail.com>
2022-10-30 10:13:11 +0800

    created user route, model & controller. Hashed password and authenticate user

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, getMe } = require('../controllers/userController');
+
+module.exports = router;
+
+router.post('/', registerUser);
+router.post('/login', loginUser);
+router.get('/me', getMe);
